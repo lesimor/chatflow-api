e7063e7cde698f525357a1cbfaabdc9a9aa0da47
lesimor <brillante91@naver.com>
2019-07-14 16:46:41 +0900

    Add command for pip install

--- deploy/gunicorn_start.sh
+++ deploy/gunicorn_start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 # You must execute in project root directory
 source /venv/bin/activate
+pip install -r requirements.txt --upgrade
 gunicorn -c /deploy/gunicorn/config.py api.wsgi:application
